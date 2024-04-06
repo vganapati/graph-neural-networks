9987b3847353806437e0947aa1b5ceee982436da
vganapati <vganapa1@swarthmore.edu>
2024-04-05 17:48:21 -0700

    Create tetris_classifier.py

---
+++ tetris_classifier.py
@@ -0,0 +1,35 @@
+"""
+* -- *
+|    |
+* -- *
+
+     *
+     |
+* -- * -- *
+
+*
+|
+*
+|
+*
+|
+*
+
+(assume flips are the same shape)
+* -- *
+|
+*
+|
+*
+
+
+(assume flips are the same shape)
+     *
+     |
+* -- *
+|
+*
+
+GCN to classify vs. E(2) network
+"""
+
