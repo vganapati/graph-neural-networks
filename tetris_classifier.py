bf552f5644b045526a0026ad234533c30dcc492e
vganapati <vganapa1@swarthmore.edu>
2024-04-09 17:03:05 -0700

    Update tetris_classifier.py

--- tetris_classifier.py
+++ tetris_classifier.py
@@ -61,21 +61,6 @@ from torch_geometric.nn import GCNConv, global_add_pool
 edge_index = torch.tensor([[0, 0, 0, 1, 1, 2, 1, 2, 3, 2, 3, 3],
                            [1, 2, 3, 2, 3, 3, 0, 0, 0, 1, 1, 2]], dtype=torch.long)
 
-edge_index_0 = torch.tensor([[0, 1, 2, 3, 1, 2, 3, 0],
-                            [1, 2, 3, 0, 0, 1, 2, 3,]], dtype=torch.long)
-
-edge_index_1 = torch.tensor([[0, 1, 1, 1, 3, 2],
-                            [1, 3, 2, 0, 1, 1]], dtype=torch.long)
-
-edge_index_2 = torch.tensor([[0, 1, 2, 1, 2, 3],
-                            [1, 2, 3, 0, 1, 2]], dtype=torch.long)
-
-edge_index_3 = torch.tensor([[0, 1, 2, 1, 2, 3],
-                            [1, 2, 3, 0, 1, 2]], dtype=torch.long)
-
-edge_index_4 = torch.tensor([[0, 1, 2, 1, 2, 3],
-                            [1, 2, 3, 0, 1, 2]], dtype=torch.long)
-
 node_attributes_0 = torch.tensor([[0, 0, 0],
                                   [1, 0, 0],
                                   [1, 1, 0],
@@ -114,38 +99,56 @@ data_list = [data_0, data_1, data_2, data_3, data_4]
 data_loader = DataLoader(data_list, batch_size=batch_size, shuffle=False)
 
 
-# Transformation
+def create_transform_mat():
+    """
+    Random transformation by 3D rotation and translation in a 5x5x5 box
+    3D rotation, uniform on unit sphere: https://paulbourke.net/geometry/randomvector/
+    u is uniformly randomly distributed from 0 to 1
+    """
 
-# Random transformation by 3D rotation and translation in a 5x5x5 box
+    u = torch.rand(1)
 
-# 3D rotation, uniform on unit sphere: https://paulbourke.net/geometry/randomvector/
+    theta_x = torch.acos(2*u - 1)
+    theta_z = 2*np.pi*u
 
-# u is uniformly randomly distributed from 0 to 1
-u = torch.rand(1)
+    # perform x rotation first
+    rot_x_mat = torch.tensor([[1, 0, 0],
+                            [1, torch.cos(theta_x), -torch.sin(theta_x)],
+                            [0, torch.sin(theta_x), torch.cos(theta_x)]])
 
-theta_x = torch.acos(2*u - 1)
-theta_z = 2*np.pi*u
+    rot_z_mat = torch.tensor([[torch.cos(theta_z), -torch.sin(theta_z), 0],
+                            [torch.sin(theta_z), torch.cos(theta_z), 0],
+                            [0, 0, 1]])
 
-# perform x rotation first
-rot_x_mat = torch.tensor([[1, 0, 0],
-                          [1, torch.cos(theta_x), -torch.sin(theta_x)],
-                          [0, torch.sin(theta_x), torch.cos(theta_x)]])
+    # translation vector
+    translate_x = 5*torch.rand(1)
+    translate_y = 5*torch.rand(1)
+    translate_z = 5*torch.rand(1)
 
-rot_z_mat = torch.tensor([[torch.cos(theta_z), -torch.sin(theta_z), 0],
-                          [torch.sin(theta_z), torch.cos(theta_z), 0],
-                          [0, 0, 1]])
-
-# translation vector
-translate_x = 5*torch.rand(1)
-translate_y = 5*torch.rand(1)
-translate_z = 5*torch.rand(1)
-
-translate_vec = torch.tensor([[translate_x],
-                              [translate_y],
-                              [translate_z]])
-
+    translate_vec = torch.tensor([[translate_x],
+                                [translate_y],
+                                [translate_z]])
+    return (rot_z_mat @ rot_x_mat, translate_vec)
 
 # Classification
+
+class GCN_equivariant(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(num_features, hidden_channels)
+        self.lin = Linear(hidden_channels, num_classes)
+    def conv_equivariant(self, x, edge_index):
+        pass
+        # reference: https://pytorch-geometric.readthedocs.io/en/latest/notes/create_gnn.html
+    def forward(self, x, edge_index, batch, dropout_prob=0):
+        # obtain node embeddings
+        x = self.conv1(x, edge_index)
+        # readout layer
+        x = global_add_pool(x, batch) # output shape is [batch_size, hidden_channels]
+        # final classifier
+        x = F.dropout(x, p=dropout_prob, training=self.training)
+        x = self.lin(x)
+        return x
 
 class GCN(torch.nn.Module):
     def __init__(self, hidden_channels):
@@ -154,6 +157,7 @@ class GCN(torch.nn.Module):
         self.lin = Linear(hidden_channels, num_classes)
     def forward(self, x, edge_index, batch, dropout_prob=0):
         # obtain node embeddings
+        breakpoint()
         x = self.conv1(x, edge_index)
         # readout layer
         x = global_add_pool(x, batch) # output shape is [batch_size, hidden_channels]
@@ -179,25 +183,25 @@ def train():
     return total_loss
 
 def test(transform=True):
+    if transform:
+        rot_mat, translate_vec = create_transform_mat()
     model.eval()
     correct = 0
     for data in data_loader:
-        # XXX transform x features
-        out = model(data.x, data.edge_index, data.batch)
+        if transform:
+            out = model(((rot_mat @ data.x.T) + translate_vec).T, data.edge_index, data.batch)
+        else:
+            out = model(data.x, data.edge_index, data.batch)
         pred = out.argmax(dim=1)
         correct += int((pred == data.y).sum())
     return correct / len(data_loader.dataset)
 
-num_epochs = 100
+num_epochs = 200
 
 for epoch in range(num_epochs):
     total_loss = train()
-    # i=0
-    # for name, param in model.state_dict().items():
-    #     if i==1:
-    #         print(name, param)
-    #         break
-    #     i += 1
     train_acc = test(transform=False)
     test_acc = test(transform=True)
     print(f'Epoch: {epoch:03d}, Loss: {total_loss:.4f}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+
+# Tetris conversion
